7a4af28739a3af6edf4a3f62df055a0ab5dd6158
Mahmoud <oudy_mosa@hotmail.com>
2024-07-01 09:26:38 +0300

    add event-details + fix some issues + add features

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -44,3 +44,52 @@ $(".mission-vision .inner-cont >div").click(function () {
   $(this).toggleClass("activ").removeClass("remove-cont");
 
 });
+
+
+
+
+
+// top functions
+
+$(document).ready(function () {
+
+  // color func
+  $(".black-white").on("click", function (e) {
+    $("body").toggleClass("contrast");
+  
+    e.stopPropagation();
+   
+  });
+  
+  
+  
+    /* --- Font sizing Function --- */
+    $('#increase-font').click(function(){
+      modifyFontSize('html','increase');
+    });
+    $('#decrease-font').click(function(){
+      modifyFontSize('html','decrease')
+    });
+    $('#reset-font').click(function(){
+      modifyFontSize('html','reset')
+    });
+  
+  });
+  
+    function modifyFontSize(MyElement,flag){
+  
+      var HtmlElement = $(MyElement);
+      var currentFontSize = parseInt (HtmlElement.css('font-size'));
+      
+      if (flag =='increase' & currentFontSize < 18 )
+          currentFontSize += 1;
+      else if (flag == 'decrease' & currentFontSize >= 15 )
+          currentFontSize -= 1;
+      else if (flag == 'reset')
+      currentFontSize = 16;
+  
+      HtmlElement.css('font-size', currentFontSize);
+  
+      // console.log(currentFontSize);
+  
+    }
